7055a1399db836d034023fb8e9fdc159f2dde396
furudooru <160915885+furudooru@users.noreply.github.com>
2025-02-13 19:49:52 +0800

    Created pages for both recipe 1 and 2
    Created new views recipe1 and recipe2 under ledger. Renamed recipes.html to home.html and created recipe.html for the individual pages.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import ledger
+from .views import home, recipe1, recipe2
 
 urlpatterns = [
-    path('', ledger, name='ledger')
+    path('', home, name='home'),
+    path('recipe/1', recipe1, name='Recipe 1'),
+    path('recipe/2', recipe2, name='Recipe 2')
 ]
 
 app_name = "ledger"
