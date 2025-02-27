ebf3f12b6976f3d73c363ab3ad75354a24178391
furudooru <160915885+furudooru@users.noreply.github.com>
2025-02-27 17:37:54 +0800

    Implemented database and models
    Changed hard coded function views in views.py to class based views that use data from the Recipe, Ingredient, and RecipeIngredient models. Templates have been adjusted to follow.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length = 50)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("ledger:home", args = [self.pk])
+    
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ledger:recipe", args = [self.pk])
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length = 50)
+    recipe = models.ForeignKey(Recipe, on_delete = models.SET_NULL, 
+                               null = True, related_name = 'ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete = models.SET_NULL, 
+                                   null = True, related_name = 'recipe')
