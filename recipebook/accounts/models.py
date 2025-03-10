d749a55a2ff6cba827542324724257fe9faa5a85
furudooru <160915885+furudooru@users.noreply.github.com>
2025-03-10 20:55:55 +0800

    reuploaded password reset implementation

--- recipebook/accounts/models.py
+++ recipebook/accounts/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     user =  models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     short_bio = models.TextField()
+    
+    def __str__(self):
+        return f'{self.name}'
