0d8191f2cb0718fcc26568c7e7acc7de176fd79f
furudooru <160915885+furudooru@users.noreply.github.com>
2025-03-10 19:46:52 +0800

    Implemented password reset
    Also updated htmls for home and recipe to fit project specs

---
+++ ledger/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import HomeListView, RecipeDetailView
+
+urlpatterns = [
+    path('recipes/list', HomeListView.as_view(), name='home'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
+]
+
+app_name = "ledger"
